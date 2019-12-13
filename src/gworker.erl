548350a886603afc6fc4258c227ac8ef8fe78fc3
MasterGauthe <gauthier.vanvracem@student.uclouvain.be>
2019-12-13 22:17:39 +0100

    Refractoring of gworker.
    Now fully functional.

--- src/gworker.erl
+++ src/gworker.erl
@@ -1,27 +1,16 @@
-%%%-------------------------------------------------------------------
-%%% @author Igor Kopestenski
-%%% 2019, <UCLouvain>
-%%% @doc
-%%% Sample generic server demonstrating usage of the Achlys task
-%%% model API.
-%%% @end
-%%%-------------------------------------------------------------------
 -module(gworker).
 -author("Crochet Christophe, Van Vracem Gauthier").
 
 -behaviour(gen_server).
 
-%% API
 -export([start_link/0]).
 
-%% Adds the pmodnav_task to the working set
-%% using the Achlys task model
 -export([add_task_exp/0,
         add_task_press/6,
         add_task_temp/6]).
 
-%% gen_server callbacks
 -export([init/1 ,
+        show/0,
         handle_call/3 ,
         handle_cast/2 ,
         handle_info/2 ,
@@ -33,279 +22,62 @@
 -record(state , {}).
 
 %%%===================================================================
-%%% API
+%%% Default Achlys functions
 %%%===================================================================
 
-println(What) -> io:format("~p~n", [What]).
-println(Text,What) -> io:format(Text ++ "~p~n", [What]).
-
-%return the average of a list
-average(List) ->
-  lists:sum(List) / length(List).
-
-%return the variance of a list
-variance(List) ->
-  Mean = average(List),
-  NewList = lists:flatmap(fun(Elem)->
-                              [(abs(Elem-Mean))*(abs(Elem-Mean))]
-                          end, List),
-  average(NewList).
-
-%Initializes lasp's variables : global best value and global counter
-%return :
-% - Best  = global lasp set "best"
-% - Count = global lasp counter "gcountvar"
-% - S     = Set of Best
-% - Length= length of set S
-initGlobalVar(Set) ->
-  {ok, {Best, _, _, _}} = lasp:declare({<<"best">>, Set}, Set),
-  {ok, {Count, _, _, _}} = lasp:declare({<<"gcountvar">>, state_gcounter}, state_gcounter),
-  {ok , S} = lasp:query(Best),
-  {ok, Length} = lasp:query(Count),
-  {Best,Count,S,Length}.
-
-%return of the Best set value that satisfy the Mode
-getElem(Mode, Length, Best, S) ->
-  case Mode of
-    min      -> if
-                Length == 0 -> lasp:update(Best, {add, 100}, self()),
-                               Elem = 100;
-                true -> Elem = lists:min(sets:to_list(S))
-              end;
-    max      -> if
-                Length == 0 -> lasp:update(Best, {add, 0}, self()),
-                               Elem = 0;
-                true -> Elem = lists:max(sets:to_list(S))
-              end;
-    mean     -> if
-                Length == 0 -> lasp:update(Best, {add, 0}, self()),
-                               Elem = 0;
-                true -> Elem = average(sets:to_list(S))
-              end;
-    variance -> if
-                Length == 0 -> lasp:update(Best, {add, 0}, self()),
-                               Elem = 0;
-                true -> Elem = variance(sets:to_list(S))
-              end
-  end,
-  Elem.
-
-%update the global var lasp if the local variable is better than the global
-%one
-updateElem(Mode,Value,Elem,Best) ->
-  case Mode of
-    min      -> if
-                  Value < Elem -> lasp:update(Best, {add, Value}, self()),
-                                  UpdateEle = Value;
-                  true -> UpdateEle = Elem
-                end;
-    max      -> if
-                  Value > Elem -> lasp:update(Best, {add, Value}, self()),
-                                  UpdateEle = Value;
-                  true -> UpdateEle = Elem
-                end;
-    mean     -> Value2 = (Value + Elem) / 2,
-                lasp:update(Best, {add, Value2}, self()),
-                UpdateEle = (Value + Elem) / 2;
-    variance -> lasp:update(Best, {add, variance([Value,Elem])}, self()),
-                UpdateEle = variance([Value,Elem])
-end,
-{UpdateEle}.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(start_link() ->
-  {ok , Pid :: pid()} | ignore | {error , Reason :: term()}).
 start_link() ->
   gen_server:start_link({local , ?SERVER} , ?MODULE , [] , []).
 
--spec(show() ->
-  {ok , Pid :: pid()} | ignore | {error , Reason :: term()}).
-show() ->
-  EWType = state_ewflag,
-  EWVarName = <<"ewvar">>,
-  GCountType = state_gcounter,
-  GCountVarName = <<"gcountvar">>,
+add_task_exp() ->
+  gen_server:cast(?SERVER
+  , {task, task_exp()}).
 
-  {ok, {GCount, _, _, _}} = lasp:declare({GCountVarName, GCountType}, GCountType),
-  {ok, GCountRes0} = lasp:query(GCount),
-  {ok, {EW, _, _, _}} = lasp:declare({EWVarName, EWType}, EWType),
-  {ok, EWRes0} = lasp:query(EW),
+add_task_temp(Mode1,Mode2,Len,SampleRate,LB,UB) ->
+  gen_server:cast(?SERVER
+  , {task, temperature(Mode1,Mode2,Len,SampleRate,LB,UB)}).
 
-  println(GCountRes0),
-  println(EWRes0),
+add_task_press(Mode1,Mode2,Len,SampleRate,LB,UB) ->
+  gen_server:cast(?SERVER
+  , {task, pressure(Mode1,Mode2,Len,SampleRate,LB,UB)}).
 
-  {ok, {Alpha, _, _, _}} = lasp:declare({<<"best_max">>, state_orset}, state_orset),
-  {ok , Sa} = lasp:query(Alpha) ,
-  println(sets:to_list(Sa)).
+init([]) ->
+  ok = loop_schedule_task(3, 1000),
+  logger:log(critical, "Running provider ~n"),
+  {ok , #state{}}.
 
-    %%--------------------------------------------------------------------
-    %% @doc
-    %% Propagates the pmodnav_task
-    %% @end
-    %%--------------------------------------------------------------------
+handle_call(_Request , _From , State) ->
+  {reply , ok , State}.
 
-    -spec(add_task_exp() ->
-      {ok , Pid :: pid()} | ignore | {error , Reason :: term()}).
-    add_task_exp() ->
-      gen_server:cast(?SERVER
-      , {task, task_exp()}).
+handle_cast({task, Task} , State) ->
+  logger:log(critical, "Received task cast signal ~n"),
+  %% Task propagation to the cluster, including self
+  achlys:bite(Task),
+  {noreply , State};
+handle_cast(_Request , State) ->
+  {noreply , State}.
 
-    -spec(add_task_temp(_Mode1,_Mode2,_Len,_SampleRate,_LB,_UB) ->
-      {ok , Pid :: pid()} | ignore | {error , Reason :: term()}).
-    add_task_temp(Mode1,Mode2,Len,SampleRate,LB,UB) ->
-      gen_server:cast(?SERVER
-      , {task, temperature(Mode1,Mode2,Len,SampleRate,LB,UB)}).
+handle_info({task, Task} , State) ->
+  logger:log(critical, "Received task signal ~n"),
+  %% Task propagation to the cluster, including self
+  achlys:bite(Task),
+  {noreply , State};
+handle_info(_Info , State) ->
+  {noreply , State}.
 
-    -spec(add_task_press(_Mode1,_Mode2,_Len,_SampleRate,_LB,_UB) ->
-      {ok , Pid :: pid()} | ignore | {error , Reason :: term()}).
-    add_task_press(Mode1,Mode2,Len,SampleRate,LB,UB) ->
-      gen_server:cast(?SERVER
-      , {task, pressure(Mode1,Mode2,Len,SampleRate,LB,UB)}).
+terminate(_Reason , _State) ->
+  ok.
 
+code_change(_OldVsn , State , _Extra) ->
+  {ok , State}.
 
-    %%%===================================================================
-    %%% gen_server callbacks
-    %%%===================================================================
+%%%===================================================================
+%%% Exemple loop task
+%%%===================================================================
 
-    %%--------------------------------------------------------------------
-    %% @private
-    %% @doc
-    %% Initializes the server
-    %%
-    %% @spec init(Args) -> {ok, State} |
-    %%                     {ok, State, Timeout} |
-    %%                     ignore |
-    %%                     {stop, Reason}
-    %% @end
-    %%--------------------------------------------------------------------
-    -spec(init(Args :: term()) ->
-      {ok , State :: #state{}} | {ok , State :: #state{} , timeout() | hibernate} |
-      {stop , Reason :: term()} | ignore).
-    init([]) ->
-      ok = loop_schedule_task(1, 200),
-      logger:log(critical, "Running provider ~n"),
-      {ok , #state{}}.
-
-    %%--------------------------------------------------------------------
-    %% @private
-    %% @doc
-    %% Handling call messages
-    %%
-    %% @end
-    %%--------------------------------------------------------------------
-    -spec(handle_call(Request :: term() , From :: {pid() , Tag :: term()} ,
-    State :: #state{}) ->
-      {reply , Reply :: term() , NewState :: #state{}} |
-      {reply , Reply :: term() , NewState :: #state{} , timeout() | hibernate} |
-      {noreply , NewState :: #state{}} |
-      {noreply , NewState :: #state{} , timeout() | hibernate} |
-      {stop , Reason :: term() , Reply :: term() , NewState :: #state{}} |
-      {stop , Reason :: term() , NewState :: #state{}}).
-    handle_call(_Request , _From , State) ->
-      {reply , ok , State}.
-
-    %%--------------------------------------------------------------------
-    %% @private
-    %% @doc
-    %% Handling cast messages
-    %%
-    %% @end
-    %%--------------------------------------------------------------------
-    -spec(handle_cast(Request :: term() , State :: #state{}) ->
-      {noreply , NewState :: #state{}} |
-      {noreply , NewState :: #state{} , timeout() | hibernate} |
-      {stop , Reason :: term() , NewState :: #state{}}).
-    handle_cast({task, Task} , State) ->
-      logger:log(critical, "Received task cast signal ~n"),
-      %% Task propagation to the cluster, including self
-      achlys:bite(Task),
-      {noreply , State};
-    handle_cast(_Request , State) ->
-      {noreply , State}.
-
-    %%--------------------------------------------------------------------
-    %% @private
-    %% @doc
-    %% Handling all non call/cast messages
-    %%
-    %% @spec handle_info(Info, State) -> {noreply, State} |
-    %%                                   {noreply, State, Timeout} |
-    %%                                   {stop, Reason, State}
-    %% @end
-    %%--------------------------------------------------------------------
-    -spec(handle_info(Info :: timeout() | term() , State :: #state{}) ->
-      {noreply , NewState :: #state{}} |
-      {noreply , NewState :: #state{} , timeout() | hibernate} |
-      {stop , Reason :: term() , NewState :: #state{}}).
-    handle_info({task, Task} , State) ->
-      logger:log(critical, "Received task signal ~n"),
-      %% Task propagation to the cluster, including self
-      achlys:bite(Task),
-      {noreply , State};
-    handle_info(_Info , State) ->
-      {noreply , State}.
-
-    %%--------------------------------------------------------------------
-    %% @private
-    %% @doc
-    %% This function is called by a gen_server when it is about to
-    %% terminate. It should be the opposite of Module:init/1 and do any
-    %% necessary cleaning up. When it returns, the gen_server terminates
-    %% with Reason. The return value is ignored.
-    %%
-    %% @spec terminate(Reason, State) -> void()
-    %% @end
-    %%--------------------------------------------------------------------
-    -spec(terminate(Reason :: (normal | shutdown | {shutdown , term()} | term()) ,
-    State :: #state{}) -> term()).
-  terminate(_Reason , _State) ->
-    ok.
-
-  %%--------------------------------------------------------------------
-  %% @private
-  %% @doc
-  %% Convert process state when code is changed
-  %%
-  %% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-  %% @end
-  %%--------------------------------------------------------------------
-  -spec(code_change(OldVsn :: term() | {down , term()} , State :: #state{} ,
-  Extra :: term()) ->
-    {ok , NewState :: #state{}} | {error , Reason :: term()}).
-  code_change(_OldVsn , State , _Extra) ->
-    {ok , State}.
-
-  %%%===================================================================
-  %%% Internal functions
-  %%%===================================================================
-
-  schedule_task() ->
-    %% Declare an Achlys task that will be
-    %% executed exactly once
-    Task = achlys:declare(mytask
-    , all
-    , single
-    , fun() ->
-      io:format("Hello Joe ! ~n")
-    end),
-    %% Send the task to the current server module
-    %% after a 5000ms delay
-    erlang:send_after(5000, ?SERVER, {task, Task}),
-    ok.
-
-  %%%===================================================================
-
-  loop_schedule_task(0, Time) ->
+loop_schedule_task(0, Time) ->
     io:format("Finish! ~n"),
     ok;
-  loop_schedule_task(Count, Time) ->
-    %% Declare an Achlys task that will be
-    %% executed more than once
+loop_schedule_task(Count, Time) ->
     Task = achlys:declare(mytask
     , all
     , single
@@ -319,17 +91,17 @@ show() ->
     erlang:send_after(Time, ?SERVER, {task, Task}),
     ok.
 
-  %%%===================================================================
-  % Task about temperature measurements
-  %%%===================================================================
+%%%===================================================================
+%%% Task about temperature measurements
+%%%===================================================================
 
-  temperature(Mode1, Mode2, Len, SampleRate,LB,UB) ->
+temperature(Mode1, Mode2, Len, SampleRate,LB,UB) ->
     Task = achlys:declare(temperature,
     all,
     permanent,
     fun() ->
       logger:log(notice, "Reading PmodNAV temperature interval ~n"),
-      SourceId = {<<"temp_source">>, state_gset},
+      SourceId = {<<"source">>, state_gset},
       {ok , {_SourceId , _Meta , _Type , _State }} = lasp : declare (SourceId , state_gset),
 
       Buffer = lists:foldl(fun
@@ -341,9 +113,9 @@ show() ->
             Temp < UB ->
               if
                 Temp > LB -> [Temp] ++ AccIn;
-                true -> [LB]
+                true -> [LB] ++ AccIn
               end;
-            true -> [UB]
+            true -> [UB] ++ AccIn
           end
         end,[],lists:seq(1,Len)),
 
@@ -400,7 +172,7 @@ show() ->
       end).
 
 %%%===================================================================
-% Task about pressure measurements
+%%% Task about pressure measurements
 %%%===================================================================
 
 pressure(Mode1, Mode2, Len, SampleRate,LB,UB) ->
@@ -409,7 +181,7 @@ pressure(Mode1, Mode2, Len, SampleRate,LB,UB) ->
   permanent,
   fun() ->
     logger:log(notice, "Reading PmodNAV temperature interval ~n"),
-    SourceId = {<<"temp_source">>, state_gset},
+    SourceId = {<<"source">>, state_gset},
     {ok , {_SourceId , _Meta , _Type , _State }} = lasp : declare (SourceId , state_gset),
 
     Buffer = lists:foldl(fun
@@ -421,9 +193,9 @@ pressure(Mode1, Mode2, Len, SampleRate,LB,UB) ->
           Press < UB ->
             if
               Press > LB -> [Press] ++ AccIn;
-              true -> [LB]
+              true -> [LB] ++ AccIn
             end;
-          true -> [UB]
+          true -> [UB] ++ AccIn
         end
       end,[],lists:seq(1,Len)),
 
@@ -479,21 +251,20 @@ pressure(Mode1, Mode2, Len, SampleRate,LB,UB) ->
         println(" ")
     end).
 
-
 %%%===================================================================
-% Exemple Task
+%%%  Exemple Task
 %%%===================================================================
 
-    task_exp() ->
-      Task = achlys:declare(task_exp
-      , all
-      , permanent
-      , fun() ->
+task_exp() ->
+    Task = achlys:declare(task_exp
+    , all
+    , permanent
+    , fun() ->
         logger:log(notice, "Reading PmodNAV pressure interval ~n"),
         Temp = rand:uniform(100),
         Node = erlang:node(),
         {ok, {SourceId, _, _, _}} = lasp:declare({<<"source">>, state_gset}, state_gset),
-        {ok, {BestMax, _, _, _}} = lasp:declare({<<"best_max">>, state_gset}, state_gset),
+        {ok, {BestMax, _, _, _}} = lasp:declare({<<"best">>, state_gset}, state_gset),
         {ok, {Count, _, _, _}} = lasp:declare({<<"gcountvar">>, state_gcounter}, state_gcounter),
         {ok , Smax} = lasp:query(BestMax),
         {ok, Length} = lasp:query(Count),
@@ -509,14 +280,99 @@ pressure(Mode1, Mode2, Len, SampleRate,LB,UB) ->
         end,
         lasp:update(SourceId, {add, {Temp, Node}}, self()),
         lasp:update(Count, increment, self())
-    end).
-
+        end).
 
 %%%===================================================================
-% Set of useful command
+%%% Helpers functions
 %%%===================================================================
 
-%% {ok, Set} = lasp:query({<<"source">>, state_orset}), sets:to_list(Set).
-%% {ok, FarenheitSet} = lasp:query({<<"destination">>, state_orset}), sets:to_list(FarenheitSet).
-%% achlys_util:add_node('achlys1@130.104.213.164').
-%% (achlys2@130.104.213.164)3> achlys:get_all_tasks().
+show() ->
+  {ok, {Best, _, _, _}} = lasp:declare({<<"best">>, state_gset}, state_gset),
+  {ok, Best0} = lasp:query(Best),
+  io:format("BEST: " ++ "~p~n", [sets:to_list(Best0)]).
+
+%%--------------------------------------------------------------------
+
+println(What) -> io:format("~p~n", [What]).
+println(Text,What) -> io:format(Text ++ "~p~n", [What]).
+
+%%--------------------------------------------------------------------
+
+%return the average of a list
+average(List) ->
+  lists:sum(List) / length(List).
+
+%return the variance of a list
+variance(List) ->
+  Mean = average(List),
+  NewList = lists:flatmap(fun(Elem)->
+                              [(abs(Elem-Mean))*(abs(Elem-Mean))]
+                          end, List),
+  average(NewList).
+
+%%--------------------------------------------------------------------
+
+%Initializes lasp's variables : global best value and global counter
+%return :
+% - Best  = global lasp set "best"
+% - Count = global lasp counter "gcountvar"
+% - S     = Set of Best
+% - Length= length of set S
+initGlobalVar(Set) ->
+  {ok, {Best, _, _, _}} = lasp:declare({<<"best">>, Set}, Set),
+  {ok, {Count, _, _, _}} = lasp:declare({<<"gcountvar">>, state_gcounter}, state_gcounter),
+  {ok , S} = lasp:query(Best),
+  {ok, Length} = lasp:query(Count),
+  {Best,Count,S,Length}.
+
+%%--------------------------------------------------------------------
+
+%return of the Best set value that satisfy the Mode
+getElem(Mode, Length, Best, S) ->
+  case Mode of
+    min      -> if
+                Length == 0 -> lasp:update(Best, {add, 100}, self()),
+                               Elem = 100;
+                true -> Elem = lists:min(sets:to_list(S))
+              end;
+    max      -> if
+                Length == 0 -> lasp:update(Best, {add, 0}, self()),
+                               Elem = 0;
+                true -> Elem = lists:max(sets:to_list(S))
+              end;
+    mean     -> if
+                Length == 0 -> lasp:update(Best, {add, 0}, self()),
+                               Elem = 0;
+                true -> Elem = average(sets:to_list(S))
+              end;
+    variance -> if
+                Length == 0 -> lasp:update(Best, {add, 0}, self()),
+                               Elem = 0;
+                true -> Elem = variance(sets:to_list(S))
+              end
+  end,
+  Elem.
+
+%%--------------------------------------------------------------------
+
+%update the global var lasp if the local variable is better than the global
+%one
+updateElem(Mode,Value,Elem,Best) ->
+  case Mode of
+    min      -> if
+                  Value < Elem -> lasp:update(Best, {add, Value}, self()),
+                                  UpdateEle = Value;
+                  true -> UpdateEle = Elem
+                end;
+    max      -> if
+                  Value > Elem -> lasp:update(Best, {add, Value}, self()),
+                                  UpdateEle = Value;
+                  true -> UpdateEle = Elem
+                end;
+    mean     -> Value2 = (Value + Elem) / 2,
+                lasp:update(Best, {add, Value2}, self()),
+                UpdateEle = (Value + Elem) / 2;
+    variance -> lasp:update(Best, {add, variance([Value,Elem])}, self()),
+                UpdateEle = variance([Value,Elem])
+end,
+{UpdateEle}.
